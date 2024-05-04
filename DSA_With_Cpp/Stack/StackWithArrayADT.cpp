57035a48fe7609a937cebb9354f6787e9dde25f2
niladri.choudhury <niladri.choudhury@flipkart.com>
2024-05-04 21:01:40 +0530

    Create StackWithArrayADT.cpp

---
+++ DSA_With_Cpp/Stack/StackWithArrayADT.cpp
@@ -0,0 +1,67 @@
+//
+//  main.cpp
+//  StackADT
+//
+//  Created by Niladri Choudhury on 04/05/24.
+//
+
+#include <iostream>
+using namespace std;
+
+struct Stack {
+    int size;
+    int top;
+    int *arr;
+};
+
+Stack * initialize(Stack *stk) {
+    cout << "Enter stack size: ";
+    cin >> stk->size;
+    stk->top = -1;
+    stk->arr = new int[stk->size];
+    
+    return stk;
+}
+
+bool isEmpty(Stack *stk) {
+    if (stk->top == -1)
+        return true;
+    else
+        return false;
+}
+
+bool isFull(Stack *stk) {
+    if (stk->top == (stk->size-1)) {
+        return true;
+    } else return false;
+}
+
+void push(Stack *stk, int value) {
+    if (isFull(stk)) {
+        cout << "Stack Overflow";
+    } else {
+        stk->top++;
+        stk->arr[stk->top] = value;
+    }
+}
+
+int pop(Stack *stk) {
+    int x = -1;
+    if (isEmpty(stk)) {
+        cout << "stack underflow";
+    } else {
+        x = stk->arr[stk->top];
+        stk->arr[stk->top] = 0;
+        stk->top--;
+    }
+    return x;
+}
+
+int main(int argc, const char * argv[]) {
+    
+    Stack *stk = new Stack;
+    stk = initialize(stk);
+    
+    
+    return 0;
+}
