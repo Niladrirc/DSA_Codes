0b18967c9cd3645c7930edcf3c4ff7166632dfa5
niladri.choudhury <niladri.choudhury@flipkart.com>
2024-05-05 14:00:32 +0530

    Striver codes

---
+++ DSA_With_Cpp/Striver_Java/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * @author niladri.choudhury on 05/05/24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ DSA_With_Cpp/Striver_Java/src/patterns/RectangularStarPattern.java
@@ -0,0 +1,21 @@
+package patterns;
+
+import java.util.Scanner;
+
+/**
+ * @author niladri.choudhury on 05/05/24
+ */
+public class RectangularStarPattern {
+    public static void main(String[] args) {
+        System.out.print("Enter pattern size: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
