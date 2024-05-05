cd97a5f62013adcbeafc12bf9b2b1204621a46d2
niladri.choudhury <niladri.choudhury@flipkart.com>
2024-05-05 14:06:44 +0530

    Refractor

--- Striver-Codes/src/patterns/RectangularStarPattern.java
+++ Striver-Codes/src/patterns/RectangularStarPattern.java
@@ -13,7 +13,7 @@ public class RectangularStarPattern {
 
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
-                System.out.print("* ");
+                System.out.print("*  ");
             }
             System.out.println(" ");
         }
