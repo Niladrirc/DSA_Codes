5ae2f9fad05fedaebcab3fa3c08be2e42707950b
niladri.choudhury <niladri.choudhury@flipkart.com>
2024-05-08 14:11:09 +0530

    Update GCDByEuclideanAlgorithm.java

--- Striver-Codes/src/basic_maths/GCDByEuclideanAlgorithm.java
+++ Striver-Codes/src/basic_maths/GCDByEuclideanAlgorithm.java
@@ -16,6 +16,14 @@ public class GCDByEuclideanAlgorithm {
         System.out.println("The GCD of " + n1 + " and "+ n2 + " is: "+findGCD(n1, n2));
     }
 
+    /**
+     * The Algorithm
+     * The Euclidean Algorithm for finding GCD(A,B) is as follows:
+     * If A = 0 then GCD(A,B)=B, since the GCD(0,B)=B, and we can stop.
+     * If B = 0 then GCD(A,B)=A, since the GCD(A,0)=A, and we can stop.
+     * Write A in quotient remainder form (A = B⋅Q + R)
+     * Find GCD(B,R) using the Euclidean Algorithm since GCD(A,B) = GCD(B,R)
+     */
     private static int findGCD(int n, int m) {
         int max = Math.max(n, m);
         int min = Math.min(n, m);
